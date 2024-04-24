4bc6ceadbd8d78bd000baf9510c4d1a4a2bbb08a
NitinK09 <nitinkjan23@gmail.com>
2024-04-24 14:28:44 +0530

    update

---
+++ JS/code/first/script.js
@@ -0,0 +1,22 @@
+console.log("helllllooooooo")
+/*var a =Number(prompt("Enter a value"))
+// above line can also be written as var a = +prompt("Enter a value")
+confirm("are you sure")
+a = a+5;
+console.log("the value of a is:"+a)
+alert("the value of a is:"+a) */
+//take two numbers, add and print their result in console and alert
+/*var a= +prompt("Enter the value for a")
+var b= +prompt("Enter the value for b")
+confirm("are you sure")
+c = a+b;
+console.log("Addition is:"+c)*/
+// data tyes
+var d =25.36555;
+console.log(typeof(d))
+console.log(d.toFixed(2))
+console.log(d.toPrecision(3))
+console.log(d.toString(2))// converting to binary number
+console.log(d.toString(16)) //converting to hexa-decimal number
+
+
